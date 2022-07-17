a98ec7d76cbb9d2a01d49d927c7d340311cf8215
PhuongV <59261928+Ctrl-A-Ctrl-C@users.noreply.github.com>
2022-07-17 17:51:06 +0100

    Add files via upload

--- spotmicroai/motor_move_2.py
+++ spotmicroai/motor_move_2.py
@@ -10,24 +10,9 @@ i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus, address = 0x42)
 pca.frequency = 50
 
-rear_shoulder_left = None 
-rear_leg_left = None 
-rear_feet_left = None
-rear_shoulder_right = None
-rear_leg_right = None 
-rear_feet_right = None  
-front_shoulder_left = None 
-front_leg_left = None 
-front_feet_left = None
-front_shoulder_right = None 
-front_leg_right = None 
-front_feet_right = None 
-
-# motor instances, list contains empty instances
-joints = [rear_shoulder_left, rear_leg_left, rear_feet_left, rear_shoulder_right, rear_leg_right, rear_feet_right, front_shoulder_left, front_leg_left, front_feet_left, front_shoulder_right, front_leg_right, front_feet_right]
 joint_counter = 0
 
-for joint in joints:
+while joint_counter <= 12:
     joint = servo.Servo(pca.channels[motors_config_list[joint_counter]["channel"]])
     joint.set_pulse_width_range(motors_config_list[joint_counter]["min_pulse"],motors_config_list[joint_counter]["max_pulse"])
     joint.angle = int(motors_config_list[joint_counter]["rest_angle"])
