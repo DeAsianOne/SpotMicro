abf5e4bbdab0b9a8332bb46d0ed18d146ca560b6
PhuongV <59261928+Ctrl-A-Ctrl-C@users.noreply.github.com>
2022-07-26 10:54:09 +0100

    Add files via upload

--- spotmicroai/SpotClass.py
+++ spotmicroai/SpotClass.py
@@ -3,6 +3,7 @@ import busio
 from board import SCL, SDA
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
+import time
 from config import motors_config_list
 
 
@@ -56,9 +57,9 @@ class Spot():
 
     def turn_rear_right(self, shoulder_angle, leg_angle, feet_angle):
         
-        self.rear_shoulder_right.angle = 180 - shoulder_angle
-        self.rear_leg_right.angle = 180 - leg_angle
-        self.rear_feet_right.angle = 180 - feet_angle
+        self.rear_shoulder_right.angle = shoulder_angle
+        self.rear_leg_right.angle = leg_angle
+        self.rear_feet_right.angle = feet_angle
         
     def turn_front_left(self, shoulder_angle, leg_angle, feet_angle):
         
@@ -68,25 +69,25 @@ class Spot():
     
     def turn_front_right(self, shoulder_angle, leg_angle, feet_angle):
         
-        self.front_shoulder_right.angle = 180 - shoulder_angle
-        self.front_leg_right.angle = 180 - leg_angle
-        self.front_feet_right.angle = 180 - feet_angle
+        self.front_shoulder_right.angle = shoulder_angle
+        self.front_leg_right.angle = leg_angle
+        self.front_feet_right.angle = feet_angle
     
-    def turn_motor(self, shoulder_angle, leg_angle, feet_angle):
+    def turn_motor(self, rear_shoulder_left, rear_leg_left, rear_feet_left, rear_shoulder_right, rear_leg_right, rear_feet_right, front_shoulder_left, front_leg_left, front_feet_left, front_shoulder_right, front_leg_right, front_feet_right):
         
-        self.rear_shoulder_left.angle = shoulder_angle
-        self.rear_leg_left.angle = leg_angle
-        self.rear_feet_left.angle = feet_angle
+        self.rear_shoulder_left.angle = rear_shoulder_left
+        self.rear_leg_left.angle = rear_leg_left
+        self.rear_feet_left.angle = rear_feet_left
         
-        self.rear_shoulder_right.angle = 180 - shoulder_angle
-        self.rear_leg_right.angle = 180 - leg_angle
-        self.rear_feet_right.angle = 180 - feet_angle
+        self.rear_shoulder_right.angle = rear_shoulder_right
+        self.rear_leg_right.angle = rear_leg_right
+        self.rear_feet_right.angle = rear_feet_right
         
-        self.front_shoulder_left.angle = shoulder_angle
-        self.front_leg_left.angle = leg_angle
-        self.front_feet_left.angle = feet_angle
+        self.front_shoulder_left.angle = front_shoulder_left
+        self.front_leg_left.angle = front_leg_left
+        self.front_feet_left.angle = front_feet_left
         
-        self.front_shoulder_right.angle = 180 - shoulder_angle
-        self.front_leg_right.angle = 180 - leg_angle
-        self.front_feet_right.angle = 180 - feet_angle
+        self.front_shoulder_right.angle = front_shoulder_right
+        self.front_leg_right.angle = front_leg_right
+        self.front_feet_right.angle = front_feet_right
         
--- spotmicroai/move_leg_test.py
+++ spotmicroai/move_leg_test.py
@@ -11,24 +11,8 @@ def main():
         with open('3_motors.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                angles = [float(row[0]),float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8]),float(row[9]),float(row[10]),float(row[11])]
-                print(angles)
-                Spot.rear_shoulder_left.angle = angles[0]
-                Spot.rear_leg_left.angle = angles[1]
-                Spot.rear_feet_left.angle = angles[2]
-                
-                Spot.rear_shoulder_right.angle = angles[3]
-                Spot.rear_leg_right.angle = angles[4]
-                Spot.rear_feet_right.angle = angles[5]
-                
-                Spot.front_shoulder_left.angle = angles[6]
-                Spot.front_leg_left.angle = angles[7]
-                Spot.front_feet_left.angle = angles[8]
-                
-                Spot.front_shoulder_right.angle = angles[9]
-                Spot.front_leg_right.angle = angles[10]
-                Spot.front_feet_right.angle = angles[11]
-                time.sleep(0.001)
+                Spot.turn_motor(float(row[0]),float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8]),float(row[9]),float(row[10]),float(row[11]))
+                time.sleep(0.002)
             
     
     return 0
