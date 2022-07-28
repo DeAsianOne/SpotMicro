290194d9384499106d7b4edcbe1a500a61cb446f
PhuongV <59261928+Ctrl-A-Ctrl-C@users.noreply.github.com>
2022-07-28 14:38:21 +0100

    Update move_leg.py

--- spotmicroai/move_leg.py
+++ spotmicroai/move_leg.py
@@ -8,7 +8,7 @@ import SpotClass as sp
 def main():
     Spot = sp.Spot()
     while True:
-        with open('3_motors.csv', newline='') as csvfile:
+        with open('Walk_Angles.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 Spot.turn_motor(float(row[0]),float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8]),float(row[9]),float(row[10]),float(row[11]))
