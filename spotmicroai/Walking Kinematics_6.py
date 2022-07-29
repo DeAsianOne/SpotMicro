c93156d93b3509d79ef0313d45338c964d03c705
PhuongV <59261928+Ctrl-A-Ctrl-C@users.noreply.github.com>
2022-07-29 12:04:36 +0100

    Update Walking Kinematics_6.py

--- spotmicroai/Walking Kinematics_6.py
+++ spotmicroai/Walking Kinematics_6.py
@@ -95,24 +95,24 @@ def rear_left_swing(L1, L2, f):
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         x_rear_left += 0.03
         x_rear_right -= 0.01
         x_front_left -= 0.01
         x_front_right -= 0.01
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     while shoulder_rear <= 90:
         rear_leg_left, rear_feet_left = stand_phase_rear(x_rear_left, leg_height_left, L1, L2)
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         shoulder_rear += 1
         shoulder_front -= 1
         leg_height_right -= 0.2
         leg_height_left += 0.2
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
         
 def front_right_swing(L1, L2, f):
@@ -130,24 +130,24 @@ def front_right_swing(L1, L2, f):
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         shoulder_rear += 1
         shoulder_front -= 1
         leg_height_right -= 0.2
         leg_height_left += 0.2
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     while x_front_right <= 1.5:
         rear_leg_left, rear_feet_left = stand_phase_rear(x_rear_left, leg_height_left, L1, L2)
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = swing_phase_front(x_front_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         x_rear_left -= 0.01
         x_rear_right -= 0.01
         x_front_left -= 0.01
         x_front_right += 0.03
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
         
 def rear_right_swing(L1, L2, f):
@@ -165,24 +165,24 @@ def rear_right_swing(L1, L2, f):
         rear_leg_right, rear_feet_right = swing_phase_rear(x_rear_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left , shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         x_rear_left -= 0.01
         x_rear_right += 0.03
         x_front_left -= 0.01
         x_front_right -= 0.01
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left , shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     while shoulder_rear >= 90:
         rear_leg_left, rear_feet_left = stand_phase_rear(x_rear_left, leg_height_left, L1, L2)
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left , shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         shoulder_rear -= 1
         shoulder_front += 1
         leg_height_right += 0.2
         leg_height_left -= 0.2
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left , shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     
 def front_left_swing(L1, L2, f):
@@ -200,24 +200,24 @@ def front_left_swing(L1, L2, f):
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = stand_phase_front(x_front_left, leg_height_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         shoulder_rear -= 1
         shoulder_front += 1
         leg_height_right += 0.2
         leg_height_left -= 0.2
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     while x_front_left <= 1.5:
         rear_leg_left, rear_feet_left = stand_phase_rear(x_rear_left, leg_height_left, L1, L2)
         rear_leg_right, rear_feet_right = stand_phase_rear(x_rear_right, leg_height_right, L1, L2)
         front_leg_left, front_feet_left = swing_phase_front(x_front_left, L1, L2)
         front_leg_right, front_feet_right = stand_phase_front(x_front_right, leg_height_right, L1, L2)
+        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
+        writer.writerow(row)
         x_rear_left -= 0.01
         x_rear_right -= 0.01
         x_front_left += 0.03
         x_front_right -= 0.01
-        row = [shoulder_front, rear_leg_left, rear_feet_left, shoulder_front, 180 - (rear_leg_right), 180 - (rear_feet_right), shoulder_rear, front_leg_left, front_feet_left, shoulder_rear, 180 - (front_leg_right), 180 - (front_feet_right)]
-        writer.writerow(row)
         print(row)
     
 leg_move(L1, L2)
